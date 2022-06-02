7729f89a3582f05317c194ba5478d0585d2fa5d1
Thiago Henrique Ferreira <henriquethiago298@gmail.com>
2022-06-02 02:10:45 -0300

    Feat/time spent to book called (#4)
    * starting time to reserve ticket
    
    * add time to reserve ticket

--- src/controllers/dashboardController.py
+++ src/controllers/dashboardController.py
@@ -4,6 +4,7 @@ import src.connectDb as connectDb
 import src.models.users.countUsers as countUsers
 import src.models.tickets.countTickets as countTickets
 import src.models.tickets.ticketsPerProblem as ticketsPerProblem
+import src.models.tickets.timeToReserveTicket as timeToReserveTicket
 import src.models.tickets.timeToDone as timeToDone
 
 db = connectDb.connect()
@@ -14,12 +15,14 @@ def report():
   cursorUser = userCollection.find()
   cursorTicket = ticketCollection.find()
   cursorProblem = ticketCollection.find()
+  cursorTimeReserve = ticketCollection.find({'reservedAt': {'$exists': True}})
   cursorTimeToDone = ticketCollection.find({"status":"done"})
 
   organizedJson = {
     "users": countUsers.execute(cursorUser),
     "tickets": countTickets.execute(cursorTicket),
     "tickets_per_problem": ticketsPerProblem.execute(cursorProblem),
+    "tickets_time_to_reserve": timeToReserveTicket.execute(cursorTimeReserve),
     "timeToDone": timeToDone.execute(cursorTimeToDone)
   }
 
