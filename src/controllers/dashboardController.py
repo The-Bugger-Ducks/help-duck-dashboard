0f7159e84633800a5420344ebc7569a1c3a7ca01
Thiago Henrique Ferreira <henriquethiago298@gmail.com>
2022-06-02 00:05:22 -0300

    created ticket analisys (#2)

--- src/controllers/dashboardController.py
+++ src/controllers/dashboardController.py
@@ -2,18 +2,20 @@ import simplejson as json
 
 import src.connectDb as connectDb
 import src.models.users.countUsers as countUsers
+import src.models.tickets.countTickets as countTickets
 import src.models.tickets.ticketsPerProblem as ticketsPerProblem
 
 db = connectDb.connect()
 userCollection = db.user
-ticketsCollection = db.ticket
+ticketCollection = db.ticket
 
 def report():
   cursorUser = userCollection.find()
-  cursorTickets = ticketsCollection.find()
+  cursorTicket = ticketCollection.find()
 
   organizedJson = {
     "users": countUsers.execute(cursorUser),
+    "tickets": countTickets.execute(cursorTicket),
     "tickets_per_problem": ticketsPerProblem.execute(cursorTickets)
   }
 
