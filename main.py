2980a357cb3b4546a8fa1b445fe9c9adab3dd093
GabrielCamargoL <gabriel.cleite@outlook.com>
2022-06-01 21:20:39 -0300

    add flask_restfull AP module

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_restful import Api
 
 from src.routes.exampleRoutes import example
 
 app = Flask(__name__)
+api = Api(app)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
