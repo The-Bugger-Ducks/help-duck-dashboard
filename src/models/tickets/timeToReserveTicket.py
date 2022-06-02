dd82fd4e7302c9c946a28a3f102baf598a7c85da
thhenrique <henriquethiago298@gmail.com>
2022-06-02 02:07:50 -0300

    add time to reserve ticket

--- src/models/tickets/timeToReserveTicket.py
+++ src/models/tickets/timeToReserveTicket.py
@@ -2,11 +2,28 @@ from datetime import datetime
 
 def execute(cursorTicket):
   
-  for ticket in cursorTicket:
-    reservedAt = datetime.timestamp(ticket["reservedAt"])
-    createdAt = datetime.timestamp(ticket["createdAt"])
+  sumReservedAtTimestamp= 0
+  sumCreatedTimestamp= 0
 
-    time = createdAt - reservedAt
-    
-    
-  return {}
+  diff = 0
+  media = 0
+  size = 0
+
+  for ticket in cursorTicket:
+    size+=1
+    sumCreatedTimestamp += datetime.timestamp(ticket["createdAt"]) 
+    sumReservedAtTimestamp += datetime.timestamp(ticket["reservedAt"]) 
+
+  diff = sumReservedAtTimestamp - sumCreatedTimestamp
+  media = (diff)/size
+  
+  segundos_rest = 0
+
+  horas = media // 3600
+  segundos_rest = media % 3600
+  minutos = segundos_rest / 60
+   
+  if (minutos < 10):
+    return f'{int(horas)}h:0{round(minutos)}min'
+
+  return f'{int(horas)}h:{round(minutos)}min'
