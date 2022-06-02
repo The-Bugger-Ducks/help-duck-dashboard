dc12d460b8aa328a595fdb54ba280248fddc5469
Gabriel Camargo Leite <gabriel.cleite@outlook.com>
2022-06-02 01:59:04 -0300

    add timeToDone ticket analysis (#3)

--- src/controllers/dashboardController.py
+++ src/controllers/dashboardController.py
@@ -4,6 +4,7 @@ import src.connectDb as connectDb
 import src.models.users.countUsers as countUsers
 import src.models.tickets.countTickets as countTickets
 import src.models.tickets.ticketsPerProblem as ticketsPerProblem
+import src.models.tickets.timeToDone as timeToDone
 
 db = connectDb.connect()
 userCollection = db.user
@@ -13,11 +14,13 @@ def report():
   cursorUser = userCollection.find()
   cursorTicket = ticketCollection.find()
   cursorProblem = ticketCollection.find()
+  cursorTimeToDone = ticketCollection.find({"status":"done"})
 
   organizedJson = {
     "users": countUsers.execute(cursorUser),
     "tickets": countTickets.execute(cursorTicket),
-    "tickets_per_problem": ticketsPerProblem.execute(cursorProblem)
+    "tickets_per_problem": ticketsPerProblem.execute(cursorProblem),
+    "timeToDone": timeToDone.execute(cursorTimeToDone)
   }
 
   return json.dumps(organizedJson)
